e704e804e3f14f97c596f148d730d814a4471e7f
hligaty <1970316213@qq.com>
2025-12-22 21:38:06 +0800

    refactor: Remove unnecessary @SuppressWarnings("serial") annotations from exception classes

--- src/main/java/io/github/hligaty/haibaracp/core/PoolException.java
+++ src/main/java/io/github/hligaty/haibaracp/core/PoolException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2023 hligaty
+ * Copyright 2021-2025 hligaty
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,7 +24,6 @@ import org.springframework.core.NestedRuntimeException;
  *
  * @author hligaty
  */
-@SuppressWarnings("serial")
 public class PoolException extends NestedRuntimeException {
 
     /**
